781c7ee3920792d275cf2486568fb3b0119091fb
Nico Hoffmann <github@ohnehin.com>
2015-01-06 22:22:18 +0100

    Added field method and global option

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 Kirby Footnotes v0.1
 ============
 
-This plugin extends [Kirby 2 CMS](http://getkirby.com) with some basic footnote functionalities.  
+This plugin extends [Kirby 2 CMS](http://getkirby.com) with some basic footnote functionalities.
 
 In-text reference:  
 ![In-text reference](https://cloud.githubusercontent.com/assets/3788865/5635753/670ccacc-95ec-11e4-81b8-7cdc20b077b2.png)
@@ -21,16 +21,22 @@ Footnotes list at the end of the text:
 # Update
 1. Replace the `site/plugins/footnotes` directory with recent version
 
+# Options
+Footnotes can be used either as method on a text field, e.g. `$page->text()->footnotes()->kirbytext()`, when creating templates - or globally set for all Kirbytext outputs. To do the latter add the following to your `site/config/config.php`:
+```php
+c::set('footnotes.global', true);
+```
+
 # Usage
 To add a footnote to your Kirbytext field, just include `[#. Footnote text]` at the place you want the reference number to appear (e.g. `[1. This is a footnote]` or `[7. This is another one **yeah**]`):
 ```
-“Transmigrants have multiple identities which are grounded in more than one society and thus, in effect, 
-they have a hybridized transnational identity. [...] In a deterritorialized context, the conventional 
-one-to-one relationship between state and territory is increasingly questioned and challenged” [1. Wong, 
+“Transmigrants have multiple identities which are grounded in more than one society and thus, in effect,
+they have a hybridized transnational identity. [...] In a deterritorialized context, the conventional
+one-to-one relationship between state and territory is increasingly questioned and challenged” [1. Wong,
 L. (2002): Home away from home? Abingdon: Routledge. Seite 171]
 ```
 
-**Include footnote in the list, but no reference number in the text:**  
+**Include footnote in the list, but no reference number in the text:**
 To have a footnote / an information included in the footnotes list at the end of the text, but not as a reference number inside the text, just prepend a `<no>` tag to the footnote:
 ```
 [0. <no>**Photo credits:** (link:http://www.flickr.com/photos/cubagallery/ text:Cuba Gallery)]
--- site/plugins/footnotes/footnotes.php
+++ site/plugins/footnotes/footnotes.php
@@ -1,37 +1,56 @@
 <?php
 
-kirbytext::$pre[] = function($kirbytext, $value) {
-
+function convert_footnotes($text) {
   $n = 1;
   $notes = array();
-  if (preg_match_all('/\[(\d+\. .*?)\]/s', $value, $matches)) {
+  if (preg_match_all('/\[(\d+\. .*?)\]/s', $text, $matches)) {
     foreach ($matches[0] as $fn) {
         $note = preg_replace('/\[\d+\. (.*?)\]/s', '\1', $fn);
         $notes[$n] = $note;
 
         if (substr($notes[$n],0,4) == '<no>') :
           $notes[$n] = str_replace('<no>', "", $notes[$n]);
-          $value = str_replace($fn, "", $value);
+          $text = str_replace($fn, "", $text);
         else :
-          $value = str_replace($fn, '<sup class="footnote"><a href="#fn-'.$n.'" id="fnref-'.$n.'">'.$n.'</a></sup>', $value);
+          $text = str_replace($fn, '<sup class="footnote"><a href="#fn-'.$n.'" id="fnref-'.$n.'">'.$n.'</a></sup>', $text);
         endif;
         $n++;
     }
 
 
     if (true) { // TODO: if is single article
-        $value .= "<div class='footnotes' id='footnotes'>";
-        $value .= "<div class='footnotedivider'></div>";
+        $text .= "<div class='footnotes' id='footnotes'>";
+        $text .= "<div class='footnotedivider'></div>";
 
-        $value .= "<ol>";
+        $text .= "<ol>";
         for ($i=1; $i<$n; $i++) {
-            $value .= "<li id='fn-".$i."'>".$notes[$i]." <span class='footnotereverse'><a href='#fnref-".$i."'>&#8617;</a></span></li>";
+            $text .= "<li id='fn-".$i."'>".$notes[$i]." <span class='footnotereverse'><a href='#fnref-".$i."'>&#8617;</a></span></li>";
         }
-        $value .= "</ol>";
-        $value .= "</div>";
+        $text .= "</ol>";
+        $text .= "</div>";
     }
+
+    return $text;
   }
 
   return $value;
+}
 
+
+/**
+ * Adding an footnotes field method: e.g. $page->text()->footnotes()->kirbytext()
+ */
+field::$methods['footnotes'] = function($field) {
+  $field->value = convert_footnotes($field->value);
+  return $field;
 };
+
+/**
+ *  Pre-filtering Kirbytext if option "footnotes.global" is set true
+ */
+if(c::get('footnotes.global', false)) {
+  kirbytext::$pre[] = function($kirbytext, $value) {
+    return convert_footnotes($value);
+  };
+}
+
