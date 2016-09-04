85ae45b3e0d3abafe25e2722513cc15f2779ab81
Nico Hoffmann <on-github@nhoffmann.com>
2016-09-04 17:41:45 +0200

    Fix #16

--- snippets/entry.php
+++ snippets/entry.php
@@ -2,5 +2,5 @@
   <li id="fn-<?= $count ?>" style="list-style-type:none"><?= $note ?></li>
 
 <?php else : ?>
-  <li id="fn-<?= $count ?>" value="<?= $number ?>"><?= $note ?> <span class="footnotereverse"><a href="#fnref-<?= $count ?>">&#8617;</a></span></li>
+  <li id="fn-<?= $count ?>" value="<?= $order ?>"><?= $note ?> <span class="footnotereverse"><a href="#fnref-<?= $count ?>">&#8617;</a></span></li>
 <?php endif ?>
