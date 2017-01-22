5c09d79242946ef7a260250a467c793d61cfdef5
Nico Hoffmann <on-github@nhoffmann.com>
2017-01-22 11:56:16 +0100

    Cleaned up HTML in snippets

--- snippets/entry.php
+++ snippets/entry.php
@@ -1,6 +1,10 @@
 <?php if($hide) : ?>
-  <li id="fn-<?= $count ?>" style="list-style-type:none"><?= $note ?></li>
+  <li id="fn-<?= $count ?>" style="list-style-type:none">
+    <?= $note ?>
+  </li>
 
 <?php else : ?>
-  <li id="fn-<?= $count ?>" value="<?= $order ?>"><?= $note ?> <span class="footnotereverse"><a href="#fnref-<?= $count ?>">&#8617;</a></span></li>
+  <li id="fn-<?= $count ?>" value="<?= $order ?>">
+    <?= $note ?> <span class="footnotereverse"><a href="#fnref-<?= $count ?>">&#8617;</a></span>
+  </li>
 <?php endif ?>
